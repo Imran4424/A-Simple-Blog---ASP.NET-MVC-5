49d8ad968634f0fd74a544660bcb1d1ff1cc8c3a
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-31 16:36:37 +0600

    simple blog

---
+++ SimpleBlog/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleBlog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+    }
+}
