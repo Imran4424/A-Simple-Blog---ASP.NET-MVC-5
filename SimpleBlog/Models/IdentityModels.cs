f285634d95d5fb3c203353a14778fce293ddff92
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-02 00:18:24 +0600

    simple blog

--- SimpleBlog/Models/IdentityModels.cs
+++ SimpleBlog/Models/IdentityModels.cs
@@ -20,6 +20,9 @@ namespace SimpleBlog.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+
+        public DbSet<Post> Posts { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
