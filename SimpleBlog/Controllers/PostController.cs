ca8ef7c2469ea234991d345047bf58cf6bf3e5f3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-02 07:27:47 +0600

    simple blog

--- SimpleBlog/Controllers/PostController.cs
+++ SimpleBlog/Controllers/PostController.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity;
+using SimpleBlog.Models;
 
 namespace SimpleBlog.Controllers
 {
     public class PostController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public PostController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         // GET: Controller / Index 
         public ActionResult Index()
         {
