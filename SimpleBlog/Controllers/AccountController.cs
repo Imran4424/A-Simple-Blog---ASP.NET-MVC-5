376167e2f97e0780561d74e20446d8cf0a441d50
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-02 07:20:14 +0600

    simple blog

--- SimpleBlog/Controllers/AccountController.cs
+++ SimpleBlog/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace SimpleBlog.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Name = model.Name, Country = model.Country };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
