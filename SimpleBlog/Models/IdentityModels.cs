703c649346fffcdb5f6cf401a4c1bc37c26c892e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-08 07:03:30 +0600

    blog comment

--- SimpleBlog/Models/IdentityModels.cs
+++ SimpleBlog/Models/IdentityModels.cs
@@ -30,6 +30,8 @@ namespace SimpleBlog.Models
 
         public DbSet<Post> Posts { get; set; }
 
+        public DbSet<Comment> Comments { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
